32d2f5f71a14c0c8fec0f5dbe936f0dfd11ce8a2
Aabhas Kumar Jha <abhasjha03@gmail.com>
2023-04-25 08:26:42 +0530

    new model required

--- src/models.py
+++ src/models.py
@@ -5,30 +5,27 @@ class User(db.Model):
     # Global data for all
 
     id = db.Column(db.Integer, primary_key = True)
-    user_id = db.Column(db.String(64), unique = True, nullable = False)
-    email = db.Column(db.Text, unique = True, nullable = False)
+    user_id = db.Column(db.String(64), primary_key = True)
+    email = db.Column(db.Text, primary_key = True)
 
     user_type = db.Column(db.Integer, nullable = False)
 
     password = db.Column(db.String(64), nullable = False)
 
     name = db.Column(db.Text, nullable = False)
-    phone = db.Column(db.String(10), nullable = False)
+    phone = db.Column(db.Text, nullable = False)
 
     # Student user - null if not a student user
 
-    dob = db.Column(db.String(10), nullable = True) # format : yyyy-mm-dd
+    dob = db.Column(db.Text, nullable = False) # format : yyyy-mm-dd
 
     father_name = db.Column(db.Text, nullable = True)
     father_email = db.Column(db.Text, nullable = True)
-    father_phone = db.Column(db.String(10), nullable = True)
+    father_phone = db.Column(db.Text, nullable = True)
 
     mother_name = db.Column(db.Text, nullable = True)
     mother_email = db.Column(db.Text, nullable = True)
-    mother_phone = db.Column(db.String(10), nullable = True)
-    
-    special_needs = db.Column(db.Text, nullable = True)
-    other_remarks = db.Column(db.Text, nullable = True)
+    mother_phone = db.Column(db.Text, nullable = True)
 
     # Parent user specific - null if not a parent user
 
@@ -37,7 +34,7 @@ class User(db.Model):
     spouse_relation = db.Column(db.Text, nullable = True)
     spouse_name = db.Column(db.Text, nullable = True)
     spouse_email = db.Column(db.Text, nullable = True)
-    spouse_phone = db.Column(db.String(10), nullable = True)
+    spouse_phone = db.Column(db.Text, nullable = True)
 
     # Organisation / teacher user - use the global user data
 
